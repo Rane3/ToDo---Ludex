8548690d0964c363aa6eeeeb4a4cd5311c8ea733
S0UR4Y <rayanekna@gmail.com>
2025-02-13 23:56:53 -0500

    Add better styling for navbar

--- my-todo-app/src/components/ListsNav.tsx
+++ my-todo-app/src/components/ListsNav.tsx
@@ -1,43 +1,45 @@
 import React, { FC, useState } from 'react';
 import { TodoList } from '../types/todo';
+import  '../styles/NavBarStyles.css';
 
 type TodoListProps = {
-    toDoLists: TodoList[]; 
-    setToDoLists: React.Dispatch<React.SetStateAction<TodoList[]>>; 
+    toDoLists: TodoList[];
+    setToDoLists: React.Dispatch<React.SetStateAction<TodoList[]>>;
 };
 
-const ListNav: FC<TodoListProps> = ({ toDoLists, setToDoLists }) => { 
+const ListNav: FC<TodoListProps> = ({ toDoLists, setToDoLists }) => {
     const [newListName, setNewListName] = useState('');
 
     const addTodoList = () => {
         if (!newListName.trim()) return; // Prevent adding empty list names
-        
+
         const newList = {
             id: crypto.randomUUID(),
             name: newListName,
             todoItems: [],
         };
 
-        setToDoLists((prevLists) => [...prevLists, newList]); 
+        setToDoLists((prevLists) => [...prevLists, newList]);
 
-        setNewListName(''); 
+        setNewListName('');
     };
 
     return (
         <nav id='list-nav'>
-            <h2>Todo List Navigation</h2>
-            <ul>
-                {toDoLists.map((list) => ( 
-                    <li key={list.id}>{list.name}</li>
-                ))}
-            </ul>
-            <input
-                type='text'
-                value={newListName}
-                onChange={(e) => setNewListName(e.target.value)}
-                placeholder='New list name'
-            />
-            <button onClick={addTodoList}>Add Empty List</button>
+            <div id='nav-top'>
+
+                <ul>
+                    {toDoLists.map((list) => (
+                        <li key={list.id}>{list.name}</li>
+                    ))}
+                </ul>
+            </div>
+            <div id='nav-bottom'>
+                <input type="text" 
+                    value={newListName}
+                    onChange={(e) => setNewListName(e.target.value)} name="text" className="custom-input" placeholder="Add List"></input>
+                <button className='add-button' onClick={addTodoList}>+</button>
+            </div>
         </nav>
     );
 };
